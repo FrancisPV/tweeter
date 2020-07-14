4252a3c7b9e21e4227d0f94e1293a4f7868cba31
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-14 16:55:00 -0400

    when you hover the article a name appears

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,18 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  $('.shared-tweet').on("mouseenter", function() {
+    console.log('entered')
+    // if ($("#tweeter-name").hasClass("tweeter-name")) {
+      $(this).children().children("#tweeter-name").removeClass("tweeter-name");
+    // }
+  });
+  $('.shared-tweet').on("mouseleave", function() {
+    console.log('left')
+    // if (!$("#tweeter-name").hasClass("tweeter-name")) {
+      $(this).children().children("#tweeter-name").addClass("tweeter-name");
+    // }
+  });
+});
 
