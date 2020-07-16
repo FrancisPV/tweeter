5ef0e5edef775a27af85b0441325dc119ec32ead
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-16 17:24:18 -0400

    refactored

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
-
-/* const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]; */
 
 //sanitize user input
 const escape = function(str) {
@@ -38,6 +7,7 @@ const escape = function(str) {
   return div.innerHTML;
 };
 
+// creates the display of the time when the tweet was posted
 const displayDaysSince = (unixMillisec) => {
   const currentTime = new Date(). getTime();
   const diff = currentTime - unixMillisec;
@@ -55,6 +25,7 @@ const displayDaysSince = (unixMillisec) => {
   
 };
 
+//creates tweet with the html template
 const createTweetElement = (tweet) => {
   const newTweet = `<article class="shared-tweet"> 
   <header class="image-container">
@@ -77,7 +48,7 @@ const createTweetElement = (tweet) => {
   return newTweet;
 };
 
-
+//loads the tweet and prepend them
 const loadTweets = () => {
   $.ajax({
     url: 'http://localhost:8080/tweets',
@@ -129,7 +100,7 @@ $(document).ready(function() {
       });
   });
 
-
+  //add a slide down slide up when you click on this button
   $(".btn1").click(function() {
     if ($(".tweet-container").is(":visible")) {
       $(".tweet-container").slideUp();
