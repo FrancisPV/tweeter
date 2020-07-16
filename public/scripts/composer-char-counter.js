0b42b0806b97a070c8cd0bfb97541ed2da58ad98
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-16 17:25:31 -0400

    refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
+
+//countdown of the tweet char and add a red text if the countdown exceed the 140 char limit
 $(document).ready(function() {
   console.log('IM READY');
 
   console.log($('textarea[name="text"]'));
 
-  $('textarea[name="text"]').on("input", function(event) {
+  $('textarea[name="text"]').on("input", function() {
     let text = $('textarea[name="text"]').val();
     let countdown = 140;
     const element = $("#char-limit");
